d0b480e283b1cf159eb4b94023037e42864dd612
geoclicks360 <geoclicks360@gmail.com>
2025-07-28 00:29:22 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,214 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.00832761472124055,
+        "pitch": 0.2573518633460381,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.005910940000003251,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-waiting-area-1",
+      "name": "Waiting Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.23941892323579772,
+        "pitch": 0.30414311122712157,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-waiting-area-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-waiting-area-2",
+      "name": "Waiting Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.36017051852296333,
+        "pitch": 0.049715700873665725,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5999999902760464e-7,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-waiting-area-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-consultation-",
+      "name": "Consultation ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.0009760100000022476,
+        "pitch": 0,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-consultation-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-consultation-1",
+      "name": "Consultation 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.5637098379218806,
+        "pitch": 0.11403869671017652,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2874350599999307,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-consultation-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1176887299999283,
+          "pitch": 0,
+          "title": "Consultation&nbsp;",
+          "text": "Consultation Room"
+        }
+      ]
+    }
+  ],
+  "name": "Dr Nishma - Ground floor, BMK Commercial centre, ENT Associates, Falnir Rd, opposite Highland Hospital, Mangaluru, Karnataka 575002",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
